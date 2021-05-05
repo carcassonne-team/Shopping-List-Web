2fc4fa55791b035a98e36b8a2dd24070e1acbc4d
rewe999 <zymek998@gmail.com>
2021-05-05 21:36:31 +0200

    change api URL

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://projekt.shoplist.site:8080/api";
+axios.defaults.baseURL = "https://projekt.shoplist.site/api";
 axios.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
